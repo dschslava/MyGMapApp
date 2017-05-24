d7fed23c362c1a5b95aa26a67f096e5b215b3224
dschslava <stephencello1@gmail.com>
2017-05-24 10:38:02 -0700

    incremental changes
    tries, fails

--- MyGMapApp/app/src/main/java/com/example/stephenyang/mygmapapp/MapsActivity.java
+++ MyGMapApp/app/src/main/java/com/example/stephenyang/mygmapapp/MapsActivity.java
@@ -1,14 +1,24 @@
 package com.example.stephenyang.mygmapapp;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -23,11 +33,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
     }
-
-
-    /**
+    /*
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
@@ -44,6 +51,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         final LatLng birth = new LatLng(41.805609, -87.920693);
         mMap.addMarker(new MarkerOptions().position(birth).title("Born here"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(birth));
-        //mMap.addMarker(new MarkerOptions().title("Current location"));
+//        LatLng currentLocation = new LatLng(getLastLocation().)
+        //mMap.addMarker(new MarkerOptions().position().title("Current location"));
     }
 }
