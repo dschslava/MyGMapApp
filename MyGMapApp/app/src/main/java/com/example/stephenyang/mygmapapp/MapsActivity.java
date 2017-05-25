1df91f9d5ec0d9a31d422e8b4efd311d6a9396d8
dschslava <stephencello1@gmail.com>
2017-05-25 08:32:04 -0700

    try add current location
    import locationservices, change gradle, etc, etc, add code for getting current location. app now crashes.

--- MyGMapApp/app/src/main/java/com/example/stephenyang/mygmapapp/MapsActivity.java
+++ MyGMapApp/app/src/main/java/com/example/stephenyang/mygmapapp/MapsActivity.java
@@ -1,29 +1,43 @@
 package com.example.stephenyang.mygmapapp;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Debug;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.Api;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.LocationServices;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.concurrent.TimeUnit;
 
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
     private GoogleMap mMap;
+    GoogleApiClient client = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API).build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +47,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        client.connect();
     }
     /*
      * Manipulates the map once available.
@@ -51,7 +66,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         final LatLng birth = new LatLng(41.805609, -87.920693);
         mMap.addMarker(new MarkerOptions().position(birth).title("Born here"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(birth));
-//        LatLng currentLocation = new LatLng(getLastLocation().)
-        //mMap.addMarker(new MarkerOptions().position().title("Current location"));
+
+        try{
+            LatLng currentLocation = new LatLng(LocationServices.FusedLocationApi.getLastLocation(client).getLatitude(),
+                    LocationServices.FusedLocationApi.getLastLocation(client).getLongitude());
+            mMap.addMarker(new MarkerOptions().position(currentLocation).title("Current location"));
+        }
+        catch(SecurityException e){
+            Log.d("MyGMap", "no permissions");
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
     }
 }
