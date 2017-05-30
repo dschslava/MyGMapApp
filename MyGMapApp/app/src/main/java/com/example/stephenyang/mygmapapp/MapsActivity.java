a158a29257dd61341c84e4bf2389946f10077ed1
dschslava <stephencello1@gmail.com>
2017-05-30 08:38:14 -0700

    stephen yang implementing locationmanager

--- MyGMapApp/app/src/main/java/com/example/stephenyang/mygmapapp/MapsActivity.java
+++ MyGMapApp/app/src/main/java/com/example/stephenyang/mygmapapp/MapsActivity.java
@@ -1,11 +1,16 @@
 package com.example.stephenyang.mygmapapp;
 
+import android.Manifest;
 import android.content.Context;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.location.LocationManager;
 
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,6 +22,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
+    private LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
 
     @Override
@@ -27,8 +33,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        Context.getSystemService(Context.LOCATION_SERVICE);
     }
+
+    LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            // Called when a new location is found by the network location provider.
+
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+        public void onProviderEnabled(String provider) {}
+
+        public void onProviderDisabled(String provider) {}
+    };
+
     /*
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -46,12 +65,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         final LatLng birth = new LatLng(41.805609, -87.920693);
         mMap.addMarker(new MarkerOptions().position(birth).title("Born here"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(birth));
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)){
+//
+//        }
         try {
             mMap.setMyLocationEnabled(true);
+
         }
         catch (SecurityException s){
             Log.d("MyGMap", "no location permissions");
         }
+
     }
 
     public void changeMapView(View v){
@@ -60,6 +84,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         else{
             mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+        Log.d("MyGMap", "changing map view");
+    }
+
+    public void getLocation(){
+        try{
+            
+        }
+        catch (Exception e){
+            Log.d("MyGMap", "Exception caught in getLocation()");
+            e.printStackTrace();
         }
     }
 }
